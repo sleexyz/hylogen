e447396e01eef391a3badf8a4bf42bc7723afe82
Sean Lee <freshdried@gmail.com>
2016-04-06 00:28:24 -0400

    added vector space instance

--- src/Hylogen/Types.hs
+++ src/Hylogen/Types.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE DeriveFunctor#-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE Rank2Types #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE ExplicitForAll #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -12,8 +10,8 @@
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Hylogen.Types where
 
@@ -30,21 +28,77 @@ class HyloPrim v where
   vboppre :: String -> v -> v -> v
   fromVec1 :: Vec1 -> v
 
-instance HyloPrim v => Num v where
+
+
+
+-- -- Why doesn't this work?
+-- instance (HyloPrim v, s ~ Scalar v) => VectorSpace v where
+--   type Scalar (v) = Vec1
+--   a *^ b = fromVec1 a * b -- TODO: optimize!
+
+class Show a => HasX a
+class HasX a => HasY a
+class HasY a => HasZ a
+class HasZ a => HasW a
+
+
+
+data Vec1 where
+  Vec1 :: Float -> Vec1
+  V1u :: String -> Vec1
+  V1uop :: String -> Vec1 -> Vec1
+  V1uoppre :: String -> Vec1 -> Vec1
+  V1bop :: String -> Vec1 -> Vec1 -> Vec1
+  V1boppre :: String -> Vec1 -> Vec1 -> Vec1
+  X :: (HasX a) => a -> Vec1
+  Y :: (HasY a) => a -> Vec1
+  Z :: (HasZ a) => a -> Vec1
+  W :: (HasW a) => a -> Vec1
+
+
+instance VectorSpace Vec1 where
+  type Scalar Vec1 = Float
+  a *^ b = Vec1 a * b
+
+instance Show Vec1 where
+  show expr = case expr of
+    Vec1 x -> show x
+    V1u x -> x
+    V1uop u x -> u <> "(" <> show x <> ")"
+    V1uoppre u x -> "(" <> u <> show x <> ")"
+    V1bop b x y -> "(" <> show x <> " " <> b <> " " <> show y <> ")"
+    V1boppre b x y -> b <> "(" <> show x <> ", " <> show y <> ")"
+    X x ->  show x <> ".x"
+    Y x ->  show x <> ".y"
+    Z x ->  show x <> ".z"
+    W x ->  show x <> ".w"
+
+
+instance HyloPrim Vec1 where
+  type HyloConstructor Vec1 = Float
+  vec = Vec1
+  vu = V1u
+  vuop = V1uop
+  vuoppre = V1uoppre
+  vbop = V1bop
+  vboppre = V1boppre
+  fromVec1 = id
+
+instance {-# OVERLAPPING #-} Num Vec1 where
   (+) = vbop "+"
   (*) = vbop "*"
   negate = vuoppre "-"
   abs = vuop "abs"
   signum = vuop "sign"
-  fromInteger = fromVec1 . fromInteger
+  fromInteger = Vec1 . fromInteger
 
-instance HyloPrim v => Fractional v where
+instance Fractional Vec1 where
   (/) = vbop "/"
   recip = vbop "/" 1
   fromRational = fromVec1 . fromRational
 
 
-instance HyloPrim v => Floating v where
+instance Floating Vec1 where
   pi = vu "pi"
   exp = vuop "exp"
   log = vuop "log"
@@ -63,58 +117,11 @@ instance HyloPrim v => Floating v where
   acosh x = log $ x + sqrt(x**2 - 1)
   atanh x = 0.5 * log ((1 + x)/(1 - x))
 
-
-
-
-class Show a => HasX a
-class HasX a => HasY a
-class HasY a => HasZ a
-class HasZ a => HasW a
-
-
-
-data Vec1 where
-  Vec1 :: Vec1 -> Vec1
-  V1u :: String -> Vec1
-  V1uop :: String -> Vec1 -> Vec1
-  V1uoppre :: String -> Vec1 -> Vec1
-  V1bop :: String -> Vec1 -> Vec1 -> Vec1
-  V1boppre :: String -> Vec1 -> Vec1 -> Vec1
-  X :: (HasX a) => a -> Vec1
-  Y :: (HasY a) => a -> Vec1
-  Z :: (HasZ a) => a -> Vec1
-  W :: (HasW a) => a -> Vec1
-
--- instance AdditiveGroup Vec1 where
---   type Scalar (Vec1) = (Vec1)
---   (*^) = 
-
-
-
-instance Show Vec1 where
-  show expr = case expr of
-    Vec1 x -> show x
-    V1u x -> x
-    V1uop u x -> u <> "(" <> show x <> ")"
-    V1uoppre u x -> "(" <> u <> show x <> ")"
-    V1bop b x y -> "(" <> show x <> " " <> b <> " " <> show y <> ")"
-    V1boppre b x y -> b <> "(" <> show x <> ", " <> show y <> ")"
-    X x ->  show x <> ".x"
-    Y x ->  show x <> ".y"
-    Z x ->  show x <> ".z"
-    W x ->  show x <> ".w"
-
-
-instance HyloPrim Vec1 where
-  type HyloConstructor Vec1 = Vec1
-  vec = Vec1
-  vu = V1u
-  vuop = V1uop
-  vuoppre = V1uoppre
-  vbop = V1bop
-  vboppre = V1boppre
-  fromVec1 = Vec1
-
+instance AdditiveGroup Vec1 where
+  zeroV = 0
+  (^+^) = (+)
+  negateV = negate
+  (^-^) = (-)
 
 -- | Vec2:
 
@@ -126,6 +133,17 @@ data Vec2 where
   V2bop :: String -> Vec2 -> Vec2 -> Vec2
   V2boppre :: String -> Vec2 -> Vec2 -> Vec2
 
+instance HyloPrim Vec2 where
+  type HyloConstructor Vec2 = (Vec1, Vec1)
+  vec = Vec2
+  vu = V2u
+  vuop = V2uop
+  vuoppre = V2uoppre
+  vbop = V2bop
+  vboppre = V2boppre
+  fromVec1 x = Vec2 (x, x)
+
+
 instance Show Vec2 where
   show expr = case expr of
     Vec2 (x, y) -> "vec2(" <> show x <> ", " <> show y <> ")"
@@ -135,8 +153,53 @@ instance Show Vec2 where
     V2bop b x y -> "(" <> show x <> " " <> b <> " " <> show y <> ")"
     V2boppre b x y -> b <> "(" <> show x <> ", " <> show y <> ")"
 
+instance Num Vec2 where
+  (+) = vbop "+"
+  (*) = vbop "*"
+  negate = vuoppre "-"
+  abs = vuop "abs"
+  signum = vuop "sign"
+  fromInteger = fromVec1 . fromInteger
+
+instance Fractional Vec2 where
+  (/) = vbop "/"
+  recip = vbop "/" 1
+  fromRational = fromVec1 . fromRational
+
+
+instance Floating Vec2 where
+  pi = vu "pi"
+  exp = vuop "exp"
+  log = vuop "log"
+  sqrt = vuop "sqrt"
+  (**) = vboppre "pow"
+  sin = vuop "sin"
+  cos = vuop "cos"
+  tan = vuop "tan"
+  asin = vuop "asin"
+  acos = vuop "acos"
+  atan = vuop "atan"
+  sinh x = (exp x - exp (negate x))/2
+  cosh x = (exp x + exp (negate x))/2
+  tanh x = sinh x / cosh x
+  asinh x = log $ x + sqrt(x**2 + 1)
+  acosh x = log $ x + sqrt(x**2 - 1)
+  atanh x = 0.5 * log ((1 + x)/(1 - x))
+
+instance AdditiveGroup Vec2 where
+  zeroV = 0
+  (^+^) = (+)
+  negateV = negate
+  (^-^) = (-)
+
+instance VectorSpace Vec2 where
+  type Scalar Vec2 = Vec1
+  a *^ b = fromVec1 a * b
+
+
 instance HasX Vec2
 instance HasY Vec2
+
 
 -- | Vec3:
 
@@ -148,6 +211,16 @@ data Vec3 where
   V3bop :: String -> Vec3 -> Vec3 -> Vec3
   V3boppre :: String -> Vec3 -> Vec3 -> Vec3
 
+instance HyloPrim Vec3 where
+  type HyloConstructor Vec3 = (Vec1, Vec1, Vec1)
+  vec = Vec3
+  vu = V3u
+  vuop = V3uop
+  vuoppre = V3uoppre
+  vbop = V3bop
+  vboppre = V3boppre
+  fromVec1 x = Vec3 (x, x, x)
+
 instance Show Vec3 where
   show expr = case expr of
     Vec3 (x, y, z) -> "vec3(" <> show x <> ", " <> show y <> ", " <> show z <> ")"
@@ -157,17 +230,52 @@ instance Show Vec3 where
     V3bop b x y -> "(" <> show x <> " " <> b <> " " <> show y <> ")"
     V3boppre b x y -> b <> "(" <> show x <> ", " <> show y <> ")"
 
+instance Num Vec3 where
+  (+) = vbop "+"
+  (*) = vbop "*"
+  negate = vuoppre "-"
+  abs = vuop "abs"
+  signum = vuop "sign"
+  fromInteger = fromVec1 . fromInteger
+
+instance Fractional Vec3 where
+  (/) = vbop "/"
+  recip = vbop "/" 1
+  fromRational = fromVec1 . fromRational
+
+
+instance Floating Vec3 where
+  pi = vu "pi"
+  exp = vuop "exp"
+  log = vuop "log"
+  sqrt = vuop "sqrt"
+  (**) = vboppre "pow"
+  sin = vuop "sin"
+  cos = vuop "cos"
+  tan = vuop "tan"
+  asin = vuop "asin"
+  acos = vuop "acos"
+  atan = vuop "atan"
+  sinh x = (exp x - exp (negate x))/2
+  cosh x = (exp x + exp (negate x))/2
+  tanh x = sinh x / cosh x
+  asinh x = log $ x + sqrt(x**2 + 1)
+  acosh x = log $ x + sqrt(x**2 - 1)
+  atanh x = 0.5 * log ((1 + x)/(1 - x))
+
+instance AdditiveGroup Vec3 where
+  zeroV = 0
+  (^+^) = (+)
+  negateV = negate
+  (^-^) = (-)
+
+instance VectorSpace Vec3 where
+  type Scalar Vec3 = Vec1
+  a *^ b = fromVec1 a * b
+
 instance HasX Vec3
 instance HasY Vec3
 instance HasZ Vec3
-
-instance Num Vec3 where
-  (+) = V3bop "+"
-  (*) = V3bop "*"
-  negate = V3uoppre "-"
-  abs = V3uop "abs"
-  signum = V3uop "sign"
-  fromInteger = (\x -> Vec3 (x, x, x)) . fromInteger
 
 
 
@@ -182,6 +290,15 @@ data Vec4 where
   V4bop :: String -> Vec4 -> Vec4 -> Vec4
   V4boppre :: String -> Vec4 -> Vec4 -> Vec4
 
+instance HyloPrim Vec4 where
+  type HyloConstructor Vec4 = (Vec1, Vec1, Vec1, Vec1)
+  vec = Vec4
+  vu = V4u
+  vuop = V4uop
+  vuoppre = V4uoppre
+  vbop = V4bop
+  vboppre = V4boppre
+  fromVec1 x = Vec4 (x, x, x, x)
 
 instance Show Vec4 where
   show expr = case expr of
@@ -191,6 +308,49 @@ instance Show Vec4 where
     V4uoppre u x -> "(" <> u <> show x <> ")"
     V4bop b x y -> "(" <> show x <> " " <> b <> " " <> show y <> ")"
     V4boppre b x y -> b <> "(" <> show x <> ", " <> show y <> ")"
+
+instance Num Vec4 where
+  (+) = vbop "+"
+  (*) = vbop "*"
+  negate = vuoppre "-"
+  abs = vuop "abs"
+  signum = vuop "sign"
+  fromInteger = fromVec1 . fromInteger
+
+instance Fractional Vec4 where
+  (/) = vbop "/"
+  recip = vbop "/" 1
+  fromRational = fromVec1 . fromRational
+
+
+instance Floating Vec4 where
+  pi = vu "pi"
+  exp = vuop "exp"
+  log = vuop "log"
+  sqrt = vuop "sqrt"
+  (**) = vboppre "pow"
+  sin = vuop "sin"
+  cos = vuop "cos"
+  tan = vuop "tan"
+  asin = vuop "asin"
+  acos = vuop "acos"
+  atan = vuop "atan"
+  sinh x = (exp x - exp (negate x))/2
+  cosh x = (exp x + exp (negate x))/2
+  tanh x = sinh x / cosh x
+  asinh x = log $ x + sqrt(x**2 + 1)
+  acosh x = log $ x + sqrt(x**2 - 1)
+  atanh x = 0.5 * log ((1 + x)/(1 - x))
+
+instance AdditiveGroup Vec4 where
+  zeroV = 0
+  (^+^) = (+)
+  negateV = negate
+  (^-^) = (-)
+
+instance VectorSpace Vec4 where
+  type Scalar Vec4 = Vec1
+  a *^ b = fromVec1 a * b
 
 instance HasX Vec4
 instance HasY Vec4
