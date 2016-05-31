4bcb3ae38939057f2664e67a3ffa9d15435969d0
Sean Lee <freshdried@gmail.com>
2016-05-30 23:36:27 -0400

    added error hud

--- src/local.js
+++ src/local.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 
 import Program from "./Program";
 import AudioControls from "./AudioControls";
+import ErrorText from "./ErrorText";
 
 const initialFsSource = `
 void main() {
@@ -11,63 +12,83 @@ void main() {
 `;
 
 const App = React.createClass({
-    getInitialState: function() {
-        return {
-            fsSource: initialFsSource,
-            showControls: false,
-            bannerClass: ""
-        };
-    },
-    componentWillMount: function() {
-        this.connectToServer();
-    },
-    componentDidMount: function() {
-        window.setTimeout(() => {
-            this.setState({bannerClass:"hidden"});
-        }, 3000);
-    },
-    connectToServer: function() {
-        var wsConn = new WebSocket("ws://localhost:8080/");
+  getInitialState: function() {
+    return {
+      fsSource: initialFsSource,
+      showControls: false,
+      bannerClass: "",
+      error: false,
+      errorText: ""
+    };
+  },
+  componentWillMount: function() {
+    this.connectToServer();
+  },
+  componentDidMount: function() {
+    window.setTimeout(() => {
+      this.setState({bannerClass:"hidden"});
+    }, 3000);
+  },
+  connectToServer: function() {
+    const wsConn = new WebSocket("ws://localhost:8080/");
 
-        wsConn.onopen = function() {
-            console.log("websocket opened!");
-        };
+    wsConn.onopen = function() {
+      console.log("websocket opened!");
+    };
 
-        wsConn.onclose = () => {
-            console.log('websocket closed');
-            window.setTimeout(this.connectToServer, 1000);
-        };
+    wsConn.onclose = () => {
+      console.log('websocket closed');
+      window.setTimeout(this.connectToServer, 1000);
+    };
 
-        wsConn.onmessage = (m) => {
-            console.log(m.data);
-            this.setState({
-                fsSource: m.data
-            });
-        };
-    },
-    toggleShow() {
-        this.setState((prev) => {return {showControls: !prev.showControls};});
-    },
-    render: function() {
-        let buttonText = this.state.showControls ? "hide" : "show";
-        let audioControlClassName = this.state.showControls ? "unhiddenFast" : "hiddenFast";
-        return (
-              <div id="entry">
-                <div id="banner" className={this.state.bannerClass}>
-                  <div id="title">Hylogen</div>
-                </div>
-                <button onClick={this.toggleShow} id="hideButton">
-                {buttonText}
-                </button>
-                <div className={audioControlClassName}>
-                <AudioControls initialState="usermedia"/>
-                </div>
-                <Program startAnimating={true}
-                         fsSource={this.state.fsSource}
-                         withOSC={true}/>
-              </div>
-        );
-    }
+    wsConn.onmessage = (m) => {
+      const obj =  JSON.parse(m.data);
+      if (obj.code !== undefined) {
+        this.setState({
+          fsSource: obj.code,
+          error: false,
+          errorText: ""
+        });
+      } else if (obj.error !== undefined) {
+        console.error(obj.error);
+        this.setState({
+          error: true,
+          errorText: obj.error,
+        });
+      } else {
+        console.error("unexpected error!");
+      }
+    };
+  },
+  toggleShow() {
+    this.setState((prev) => {return {showControls: !prev.showControls};});
+  },
+  render: function() {
+    const buttonText = this.state.showControls ? "hide" : "show";
+    const audioControlClassName = this.state.showControls ? "unhiddenFast" : "hiddenFast";
+
+    const error = this.state.error
+      ?(<div><ErrorText text={this.state.errorText}/></div>)
+      :(<div></div>);
+
+    return (
+        <div id="entry">
+        <div id="banner" className={this.state.bannerClass}>
+        <div id="title">Hylogen</div>
+        </div>
+        <button onClick={this.toggleShow} id="hideButton">
+        {buttonText}
+      </button>
+        <div className={audioControlClassName}>
+        <AudioControls initialState="usermedia"/>
+        </div>
+        {error}
+        <Program startAnimating={true}
+      fsSource={this.state.fsSource}
+      withOSC={true}/>
+        </div>
+    );
+  }
 });
 
 ReactDOM.render(<App/>, document.getElementById("entry"));
