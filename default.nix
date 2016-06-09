a5402bbf20c5628217537d2d2eae03a89cfeb151
Sean Lee <freshdried@gmail.com>
2016-06-09 18:24:18 -0400

    split with hylide

--- default.nix
+++ default.nix
@@ -4,21 +4,12 @@ let
 
   inherit (nixpkgs) pkgs;
 
-  f = { mkDerivation, aeson, base, bytestring, data-reify, filepath
-      , fsnotify, hint, http-types, process, stdenv, text, vector-space
-      , wai, warp, websockets
-      }:
+  f = { mkDerivation, base, data-reify, stdenv, vector-space }:
       mkDerivation {
         pname = "hylogen";
-        version = "0.1.3.1";
+        version = "0.1.4.0";
         src = ./.;
-        isLibrary = true;
-        isExecutable = true;
         libraryHaskellDepends = [ base data-reify vector-space ];
-        executableHaskellDepends = [
-          aeson base bytestring filepath fsnotify hint http-types process
-          text wai warp websockets
-        ];
         homepage = "https://github.com/sleexyz/hylogen";
         description = "an EDSL for live-coding fragment shaders";
         license = stdenv.lib.licenses.mit;
